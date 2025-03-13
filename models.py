233116f2e6f4996c47134b0b00878fba31c260b8
anugya22 <141493025+anugya22@users.noreply.github.com>
2025-03-14 03:05:32 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    moves = db.Column(db.Integer, nullable=False)
+    time = db.Column(db.String(10), nullable=False)
